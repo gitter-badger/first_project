6e2ca3195f869005312ba8f8e9ae6fe737536299
hawkerpl <m.m.hawker@gmail.com>
2015-04-25 11:38:42 +0200

    add login basic template, and functioning url

--- login/views.py
+++ login/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    text = "hello world"
+    title = "First template"
+    context_dict = {'text':text,'title':title}
+    return render(request, 'login/index.html', context_dict)
