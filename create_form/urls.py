76f8858f042ab4fab70c53c221164da9e58ecd1e
hawkerpl <m.m.hawker@gmail.com>
2015-05-18 12:59:36 +0200

    fix create form templates, add basic view

--- create_form/urls.py
+++ create_form/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^form_creation/', views.form_creation),
 ]
--- create_form/views.py
+++ create_form/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import Http404
+from django.http import HttpResponseBadRequest
 
 
 def index(request):
@@ -8,3 +8,14 @@ def index(request):
         return render(request, 'create_form/create_form_template.html', context)
     else:
         return render(request, 'error_not_logged_in.html')
+
+
+def form_creation(request):
+    try:
+        pst = request.POST
+
+        context = {"username": request.user.username, "form_link": str(pst)}
+    except ValueError:
+        return HttpResponseBadRequest("invalid input value")
+    else:
+        return render(request, 'create_form/form_created.html', context)
